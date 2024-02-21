67f50c24b817b83bd52f3377be3bb15276063edd
sammyg024 <71109804+sammyg024@users.noreply.github.com>
2024-02-21 09:47:36 -0500

    Update MechatronicsTestPt1.ino

--- MechTest/MechatronicsTestPt1.ino
+++ MechTest/MechatronicsTestPt1.ino
@@ -2,8 +2,9 @@
 
 myDuino robot(1);
 
-int switch1 = 0;
-int mod1 = 2;
+bool but1 = 0;
+int val = 0;
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -15,25 +16,25 @@ Serial.begin(9600);
 void loop() {
   // put your main code here, to run repeatedly:
 
-switch1 = robot.readButton(1);
-mod1 = mod1 % 2;
-Serial.println(String(mod1));
-
-if (switch1 == 1) { 
-  robot.digital(1,1);
-  //Serial.println("on");
-  delay(100);
-  mod1 = mod1 + 1 ;
-}
-else if (switch1 == 0) {
+but1 = robot.readButton(1);
+if (val % 2 == 0) {
   robot.digital(1,0);
-  //Serial.println("off");
-  delay(100);
+  but1 = robot.readButton(1);
+  if (but1 == true) {
+    val = val + 1;
+    delay(500);
+  }
+  Serial.println("off");
 }
-else {
-  //Serial.println("error");
+else if(val % 2 == 1) {
+  robot.digital(1,1);
+  but1 = robot.readButton(1);
+  if (but1 == true) {
+    val = val + 1;
+    delay(500);
+  }
+  Serial.println("on");
 }
-//robot.digital(1,0);
-//robot.moveMotor(1,1,255);
-
+//Serial.println(but1);
+//Serial.println(val);
 }
