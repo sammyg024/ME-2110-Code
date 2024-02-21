10c1b8e16940b494436c3eb237a6be24959c2e7d
sammyg024 <71109804+sammyg024@users.noreply.github.com>
2024-02-21 11:47:21 -0500

    Update MechatronicsTestPt5.ino w/pneumatics

--- MechTest/MechatronicsTestPt5.ino
+++ MechTest/MechatronicsTestPt5.ino
@@ -19,7 +19,7 @@ Serial.println(String(pot));
 //Banana Plugs
 switch1 = robot.readButton(1);
 
-while (1 == 1){
+while (switch1 == 1){
   Serial.println(String(switch1));
   Serial.println("ON");
 
@@ -33,20 +33,23 @@ while (1 == 1){
     robot.LED(2,1);
     //delay(5000);
     robot.LED(2,0);
+    robot.digital(4,1);
 
-    // Motor for testing
-    robot.moveMotor(1,1,255);
-    robot.moveMotor(2,1,0);
+    // All else off
+    robot.digital(1,0);
+    robot.digital(2,0);
+    robot.digital(3,0);
   }
   else if (pot <= 514 && pot >= 343) {
     robot.LED(1,1);
-    robot.digital(2,1);
     //delay(5000);
     robot.LED(1,0);
+    robot.digital(3,1);
 
-    // Motor for testing
-    robot.moveMotor(2,1,255);
-    robot.moveMotor(1,1,0);
+    // All else off
+    robot.digital(2,0);
+    robot.digital(1,0);
+    robot.digital(4,0);
   }
   else if (pot < 343 && pot >= 172) {
     robot.LED(2,1);
@@ -55,25 +58,29 @@ while (1 == 1){
     robot.LED(2,0);
 
     //Motor Turn offs
-    robot.moveMotor(1,1,0);
-    robot.moveMotor(2,1,0);
+    robot.digital(2,0);
+    robot.digital(3,0);
+    robot.digital(4,0);
   }
   else if (pot < 172) {
     robot.LED(1,0);
     robot.LED(2,0);
     robot.digital(2,1);
 
-    //Motor Turn Offs
-    robot.moveMotor(1,1,0);
-    robot.moveMotor(2,1,0);
+    //All else off
+    robot.digital(1,0);
+    robot.digital(3,0);
+    robot.digital(4,0);
+  }
+
   }
   else {
     robot.digital(1,0);
     robot.digital(2,0);
     robot.LED(1,0);
     robot.LED(2,0);
-    robot.moveMotor(1,1,0);
-    robot.moveMotor(2,1,0);
+    robot.digital(3,0);
+    robot.digital(4,0);
   }
 
   switch1 = robot.readButton(1);
